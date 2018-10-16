fd820ac8c807aa99fefd5304a172b0f6e041b550
serlinecai4935 <scai4935@bths.edu>
2018-10-16 14:48:13 -0400

    e triangle

---
+++ src/Etriangle.java
@@ -0,0 +1,23 @@
+public class Etriangle {
+//    public static void main(String[]args)
+    {
+        public class Etriangle extends Shape
+        {
+            private int side;
+
+            public Etriangle (int side)
+            {
+                super()
+                this.side = side;
+            }
+            public double getArea()
+            {
+                return
+            }
+            public double getPerimeter()
+            {
+                return side * 3;
+            }
+        }
+    }
+}
