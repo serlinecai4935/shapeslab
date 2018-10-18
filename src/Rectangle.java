a663fa798b764c80bbe29f00859e17df8c500dfd
isoha2749 <isoha2749@bths.edu>
2018-10-18 14:42:32 -0400

    Rectangle done with comments

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,5 @@
 public class Rectangle extends Shape
+    //defines the height and width sides of the rectangle
 {
     private int width;
     private int height;
@@ -9,12 +10,14 @@ public class Rectangle extends Shape
         this.height = height;
     }
     @Override
+    //getPerimeter multiplies the height and width together to find the area of the rectangle
     public double getArea()
     {
         return (width * height);
     }
 
     @Override
+    // getPerimeter adds each height and width together to find the perimeter of the rectangle
     public double getPerimeter()
     {
         return (height + height + width + width);
