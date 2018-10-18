1604ecf9beb9145b1870421be9d044badf6b84d6
Benjamin Wu <bwu3112@bths.edu>
2018-10-18 18:24:05 -0400

    Finished comments

--- src/Circle.java
+++ src/Circle.java
@@ -1,15 +1,19 @@
 public class Circle extends shapes
 {
-    //Ben
+    //Circle extends from main class shapes
     private int radius;
+    // Field defines radius
     public Circle(int radius)
     {
         super("Circle");
         this.radius = radius;
     }
+    //Constructor initializes value of radius for Circle
+    // Gives area of the circle
     public double getArea() {
         return 3.14 * radius * radius;
     }
+    // Gives perimeter/circumference of the circle
     public double getPerimeter() {
         return 3.14 * 2 * radius;
     }
