4120c64d3db460affb89d99c370216504c170173
serlinecai4935 <scai4935@bths.edu>
2018-10-19 14:46:01 -0400

    g

--- src/Etriangle.java
+++ src/Etriangle.java
@@ -1,18 +1,22 @@
 
         public class Etriangle extends shapes
         {
+            // Etriangle extends from main class shapes
             private int side;
-
+            // Field defines side
             public Etriangle (int side)
             {
                 super("Etriangle");
                 this.side = side;
             }
+            //constructor initializes the side of the Etriangle
+            //returns the area of Etriangle
             @Override
             public double getArea()
             {
-                return (Math.sqrt(3)/4)*(side * side);
+                return (Math.sqrt(3)/(4))*(side * side);
             }
+            //Returns the perimeter of the Etriangle
             @Override
             public double getPerimeter()
             {
